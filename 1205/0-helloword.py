8f247a6511cd1da65d4190203816d8b6a2842824
DaiSong <daisong24@163.com>
2018-12-05 17:55:02 +0800

    init project

---
+++ 1205/0-helloword.py
@@ -0,0 +1,23 @@
+#!/usr.bin.python3
+
+# Basic Hello World
+print("Hello, World")
+
+# 缩进用tab(个人爱好)
+if True:
+    print("True")
+else:
+    print("False")
+
+# 多行用\
+total = 1 + \
+        2 + \
+        3 + \
+        4
+print(total)
+
+# Number写写
+intNumber = 100
+boolNumber = False
+floatNumber = 0.003
+complexNumber = 1 + 2j
---
+++ readme.md
@@ -0,0 +1,8 @@
+## DS 重敲PYTHON 3之路
+### 目标
+* python语法
+* IO操作
+* 错误和异常
+* 面向对象
+* 公共库
+  reg/mysql/http/thread/JSON & XML/date
